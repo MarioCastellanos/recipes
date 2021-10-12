c8db92bbfdefa4420e865ad95d155f450c7a625e
mariocastellanos <maariocastellanos@gmail.com>
2021-10-11 21:14:48 -0700

    Recipie class

---
+++ lib/recipe.dart
@@ -0,0 +1,40 @@
+/// Author : Mario H.C.H.
+/// CLASSNAME: RECIPE
+/// PARAMETERS: String label: the name of the recipe
+///             String imageUrl: the location of the image asset for the
+///             given recipe.
+/// DESCRIPTION: Class designed to hold recipes, their names, and their
+///              the path to their picture.
+
+class Recipe {
+  String label;
+  String imageUrl;
+  Recipe(this.label, this.imageUrl);
+
+  static List<Recipe> samples = [
+    Recipe(
+      'Spaghetti and Meatballs',
+      'assets/spaghettiMeatballs.jpg',
+    ),
+    Recipe(
+      'Tomato Soup',
+      'assets/tomatoeBasilSoup.jpg',
+    ),
+    Recipe(
+      'Grilled Cheese',
+      'assets/grilledCheese.jpg',
+    ),
+    Recipe(
+      'Chocolate Chip Cookies',
+      'assets/chocolateChipCookies.jpg',
+    ),
+    Recipe(
+      'Taco Salad',
+      'assets/tacoSalad.jpg',
+    ),
+    Recipe(
+      'Hawaiian Pizza',
+      'assets/pizza.jpg',
+    ),
+  ];
+}
