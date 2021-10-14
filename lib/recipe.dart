0587b7596837bf3b6f24cc54f93abebb6262b0f8
mariocastellanos <maariocastellanos@gmail.com>
2021-10-14 10:31:01 -0700

    Added ingredients list and default serving size

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,3 +1,5 @@
+import 'package:recipes/ingredient.dart';
+
 /// Author : Mario H.C.H.
 /// CLASSNAME: RECIPE
 /// PARAMETERS: String label: the name of the recipe
@@ -11,32 +13,135 @@ class Recipe {
   String label;
   // path to image asset
   String imageUrl;
-  Recipe(this.label, this.imageUrl);
+  // List of ingredients to make the recipe.
+  List<Ingredient> ingredients;
+  // the number of servings made by default
+  int servings;
+
+  Recipe(this.label, this.imageUrl, this.servings, this.ingredients);
 
   static List<Recipe> samples = [
     Recipe(
       'Spaghetti and Meatballs',
       'assets/spaghettiMeatballs.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'box',
+          'Spaghetti',
+        ),
+        Ingredient(
+          4,
+          '',
+          'Frozen Meatballs',
+        ),
+        Ingredient(
+          0.5,
+          'jar',
+          'sauce',
+        ),
+      ],
     ),
     Recipe(
       'Tomato Soup',
       'assets/tomatoeBasilSoup.jpg',
+      2,
+      [
+        Ingredient(
+          1,
+          'can',
+          'Tomato Soup',
+        ),
+      ],
     ),
     Recipe(
       'Grilled Cheese',
       'assets/grilledCheese.jpg',
+      1,
+      [
+        Ingredient(
+          2,
+          'slices',
+          'Cheese',
+        ),
+        Ingredient(
+          2,
+          'slices',
+          'Bread',
+        ),
+      ],
     ),
     Recipe(
       'Chocolate Chip Cookies',
       'assets/chocolateChipCookies.jpg',
+      24,
+      [
+        Ingredient(
+          4,
+          'cups',
+          'flour',
+        ),
+        Ingredient(
+          2,
+          'cups',
+          'sugar',
+        ),
+        Ingredient(
+          0.5,
+          'cups',
+          'chocolate chips',
+        ),
+      ],
     ),
     Recipe(
       'Taco Salad',
       'assets/tacoSalad.jpg',
+      1,
+      [
+        Ingredient(
+          4,
+          'oz',
+          'nachos',
+        ),
+        Ingredient(
+          3,
+          'oz',
+          'taco meat',
+        ),
+        Ingredient(
+          0.5,
+          'cup',
+          'cheese',
+        ),
+        Ingredient(
+          0.25,
+          'cup',
+          'chopped tomatoes',
+        ),
+      ],
     ),
     Recipe(
       'Hawaiian Pizza',
       'assets/pizza.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'item',
+          'pizza',
+        ),
+        Ingredient(
+          1,
+          'cup',
+          'pineapple',
+        ),
+        Ingredient(
+          8,
+          'oz',
+          'ham',
+        ),
+      ],
     ),
   ];
 }
